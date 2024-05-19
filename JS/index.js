99be886190c38b047c4397487dd2cc54207f31ff
jayant0305 <jayantbhatia03@gmail.com>
2024-05-19 22:35:58 +0530

    updated instruction-box style

--- JS/index.js
+++ JS/index.js
@@ -2,10 +2,12 @@ const startButton = document.querySelector(".start-button");
 const quit=document.querySelector(".quit");
 const infoBox = document.querySelector(".instruction-box");
 const continueButton=document.querySelector(".continue");
+const h1=document.querySelector('h1');
 
 startButton.addEventListener("click", (e) => {
     infoBox.classList.add("activeInfo");
     startButton.hidden=true;
+    h1.hidden=true;
 });
 
 quit.addEventListener("click",(e)=>{
