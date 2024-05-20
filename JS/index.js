9cd68266ebf22285f282fbf82fb99a360246f10e
jayant0305 <jayantbhatia03@gmail.com>
2024-05-21 00:12:47 +0530

    changing layout structure of start and question page

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,13 @@
-const startButton = document.querySelector(".start-button");
+const startButton = document.querySelector(".start");
+const readyText=document.querySelector(".ready-text");
 const quit=document.querySelector(".quit");
 const infoBox = document.querySelector(".instruction-box");
 const continueButton=document.querySelector(".continue");
-const h1=document.querySelector('h1');
 
 startButton.addEventListener("click", (e) => {
     infoBox.classList.add("activeInfo");
     startButton.hidden=true;
-    h1.hidden=true;
+    readyText.hidden=true;
 });
 
 quit.addEventListener("click",(e)=>{
